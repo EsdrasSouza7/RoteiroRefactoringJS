eb320107ddcb5cf7516f889739522adc91dc4734
Es7 <115493775+EsdrasSouza7@users.noreply.github.com>
2024-11-13 11:28:23 -0300

    Commit 1 - Extração de Função
    Commit 1 - Extração de Função Corrigido

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ function gerarFaturaStr (fatura, pecas) {
   
     for (let apre of fatura.apresentacoes) {
       const peca = pecas[apre.id];
-      calcularTotalApresentacao(peca, apre);
+      let total = calcularTotalApresentacao(peca, apre);
+  
+      // créditos para próximas contratações
+      creditos += Math.max(apre.audiencia - 30, 0);
+      if (peca.tipo === "comedia") 
+         creditos += Math.floor(apre.audiencia / 5);
+  
+      // mais uma linha da fatura
+      faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      totalFatura += total;
     }
     faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
     faturaStr += `Créditos acumulados: ${creditos} \n`;
@@ -36,15 +45,7 @@ function gerarFaturaStr (fatura, pecas) {
       default:
         throw new Error(`Peça desconhecia: ${peca.tipo}`);
     }
-
-    // créditos para próximas contratações
-    creditos += Math.max(apre.audiencia - 30, 0);
-    if (peca.tipo === "comedia")
-      creditos += Math.floor(apre.audiencia / 5);
-
-    // mais uma linha da fatura
-    faturaStr += `  ${peca.nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
-    totalFatura += total;
+    return total;
   }
 }
 
