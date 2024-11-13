70385bdeab2867919f51db2b5e248b180d851e7e
Esdras S <souzaesdras859@gmail.com>
2024-11-13 17:52:41 -0300

    Commit 5 - Move Function

--- index.js
+++ index.js
@@ -1,58 +1,70 @@
 import { readFileSync } from 'fs';
 
-function gerarFaturaStr (fatura, pecas) {
-  let totalFatura = 0;
+function formatarMoeda(valor) {
+  return new Intl.NumberFormat("pt-BR",
+  { style: "currency", currency: "BRL",
+      minimumFractionDigits: 2 }).format(valor/100);}
+    
+function getPecas(pecas, apre) {
+  return pecas[apre.id];
+}
+
+function calcularCredito(apre, pecas) {
+  let creditos = 0;
+  creditos += Math.max(apre.audiencia - 30, 0);
+  if (pecas.tipo === "comedia")
+  creditos += Math.floor(apre.audiencia / 5);
+  return creditos;
+}
+
+function CalcularTotalCredito(apresentacoes, pecas){
   let creditos = 0
+  for (let apre of apresentacoes){
+    creditos += calcularCredito(apre, getPecas(pecas, apre))
+  }
+  return creditos
+}
+
+function calcularTotalApresentacao(pecas, apre) {
+  let total = 0;
+
+  switch (getPecas(pecas, apre).tipo) {
+    case "tragedia":
+      total = 40000;
+      if (apre.audiencia > 30) {
+        total += 1000 * (apre.audiencia - 30);
+      }
+      break;
+    case "comedia":
+      total = 30000;
+      if (apre.audiencia > 20) {
+        total += 10000 + 500 * (apre.audiencia - 20);
+      }
+      total += 300 * apre.audiencia;
+      break;
+    default:
+      throw new Error(`Peça desconhecia: ${getPecas(pecas, apre).tipo}`);
+  }
+  return total;
+}
+
+function CalcularTotalFatura(pecas, apresentacoes){
+  let totalFatura = 0
+  for (let apre of apresentacoes){
+    totalFatura += calcularTotalApresentacao(pecas, apre)
+  }
+  return totalFatura
+}
+
+function gerarFaturaStr (fatura, pecas) {
   let faturaStr = `Fatura ${fatura.cliente}\n`;
 
   for (let apre of fatura.apresentacoes) {
-    faturaStr += `  ${getPecas(apre).nome}: ${formatarMoeda(calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
+    faturaStr += `  ${getPecas(pecas, apre).nome}: ${formatarMoeda(calcularTotalApresentacao(pecas, apre))} (${apre.audiencia} assentos)\n`;
   }
-  faturaStr += `Valor total: ${formatarMoeda(CalcularTotalFatura())}\n`;
-  faturaStr += `Créditos acumulados: ${CalcularTotalCredito()} \n`;
+  faturaStr += `Valor total: ${formatarMoeda(CalcularTotalFatura(pecas, fatura.apresentacoes))}\n`;
+  faturaStr += `Créditos acumulados: ${CalcularTotalCredito(fatura.apresentacoes, pecas)} \n`;
   return faturaStr;
-  
-  
-  function getPecas(apresentacao) {
-    return pecas[apresentacao.id];
-  }
-
-  function CalcularTotalCredito(){
-    for (let apre of fatura.apresentacoes){
-      creditos += calcularCredito(apre, getPecas(apre))
-    }
-    return creditos
-  }
-
-  function CalcularTotalFatura(){
-    for (let apre of fatura.apresentacoes){
-      totalFatura += calcularTotalApresentacao(apre)
-    }
-    return totalFatura
-  }
-
-  function calcularTotalApresentacao(apre) {
-    let total = 0;
-
-    switch (getPecas(apre).tipo) {
-      case "tragedia":
-        total = 40000;
-        if (apre.audiencia > 30) {
-          total += 1000 * (apre.audiencia - 30);
-        }
-        break;
-      case "comedia":
-        total = 30000;
-        if (apre.audiencia > 20) {
-          total += 10000 + 500 * (apre.audiencia - 20);
-        }
-        total += 300 * apre.audiencia;
-        break;
-      default:
-        throw new Error(`Peça desconhecia: ${getPecas(apre).tipo}`);
-    }
-    return total;
-  }
 }
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
@@ -60,16 +72,3 @@ const pecas = JSON.parse(readFileSync('./pecas.json'));
 const faturaStr = gerarFaturaStr(faturas, pecas);
 console.log(faturaStr);
 
-function formatarMoeda(valor) {
-  return new Intl.NumberFormat("pt-BR",
-    { style: "currency", currency: "BRL",
-      minimumFractionDigits: 2 }).format(valor/100);
-}
-
-function calcularCredito(apre, getPecas) {
-  let creditos = 0;
-  creditos += Math.max(apre.audiencia - 30, 0);
-  if (getPecas.tipo === "comedia")
-    creditos += Math.floor(apre.audiencia / 5);
-  return creditos;
-}
